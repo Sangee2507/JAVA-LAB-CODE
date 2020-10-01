8cbe93e7da4b7be79d5075b0cab8c0227055cbcb
Sangeeta <70089298+Sangee2507@users.noreply.github.com>
2020-10-01 10:05:05 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -9,7 +9,6 @@ class Addition
 		int a = sc.nextInt();
 		System.out.println("Enter the second number::");
 		int b = sc.nextInt();
-		int result = a+b;
 		System.out.println("The sum is:" + result);
 	}
 }
